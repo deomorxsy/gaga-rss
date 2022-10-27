75eca3a619a59461b9cd60f26d2cfb3d3db580bc
asari <32879431+deomorxsy@users.noreply.github.com>
2022-10-27 05:15:09 -0300

    fix deprecated syntax, add account area interface

--- app/app.py
+++ app/app.py
@@ -5,6 +5,23 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+from flask_login import LoginManager
+from flask_login import login_required
+from flask_login import login_user
+from flask import redirect
+from flask import url_for
+
+#
+import sys
+sys.path.append('./auth/')
+import account_area
+
+#db dir
+sys.path.append('./db/')
+from mockdb import MockDBHelper as DBHelper
+from user import User
+
+
 app = Flask(__name__)
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
@@ -31,6 +48,44 @@ def add_header(response):
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=0'
     return response
+
+
+
+
+'''
+#mock, not in prod
+app = Flask(__name__)
+app.secret_key = 'ThOnEu2jEtgKqp69BRNh79+8YM3CkxnyEB1db9i1PRxyXNiO5JC4PemrDTiAaOU/HgTmNvmpWAXYyQ+TAs7SS48Z4AqRFjkt9O4'
+login_manager = LoginManager(app)
+
+@app.route("/")
+def home():
+    return render_template("landing.html")
+
+@app.route("/login", methods=["POST"], strict_slashes=False)
+def login():
+    email = request.form.get("email")
+    passwd = request.form.get("password")
+    user_passwd = DB.get_user(email)
+    if user_passwd and user_passwd == passwd:
+        user = User(email)
+        login_user(user)
+        return redirect(url_for('account'))
+    return home()
+
+@app.route("/account")
+@login_required
+def account():
+    return "Logged in!"
+
+@login_manager.user_loader
+def load_user(user_id):
+    user_passwd = DB.get_user(user_id)
+    if user_passwd:
+        return User(user_id)
+
+'''
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
--- app/auth/account_area.py
+++ app/auth/account_area.py
@@ -1,15 +1,51 @@
-from flask.ext.login import LoginManager
-from flask.ext.login import login_required
+from flask import Flask
+from flask import render_template
+from flask import request
 
+from flask_login import LoginManager
+from flask_login import login_required
+from flask_login import login_user
+from flask import redirect
+from flask import url_for
+
+#
+import sys
+sys.path.append('./auth/')
+import account_area
+
+#db dir
+sys.path.append('./db/')
+from mockdb import MockDBHelper as DBHelper
+from user import User
+
+#mock, not in prod
 app = Flask(__name__)
+app.secret_key = 'ThOnEu2jEtgKqp69BRNh79+8YM3CkxnyEB1db9i1PRxyXNiO5JC4PemrDTiAaOU/HgTmNvmpWAXYyQ+TAs7SS48Z4AqRFjkt9O4'
 login_manager = LoginManager(app)
 
 @app.route("/")
 def home():
     return render_template("landing.html")
 
+@app.route("/login", methods=["POST"], strict_slashes=False)
+def login():
+    email = request.form.get("email")
+    passwd = request.form.get("password")
+    user_passwd = DB.get_user(email)
+    if user_passwd and user_passwd == passwd:
+        user = User(email)
+        login_user(user)
+        return redirect(url_for('account'))
+    return home()
+
 @app.route("/account")
 @login_required
 def account():
     return "Logged in!"
+
+@login_manager.user_loader
+def load_user(user_id):
+    user_passwd = DB.get_user(user_id)
+    if user_passwd:
+        return User(user_id)
 
