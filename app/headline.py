d7267d1b3967b74fb55c379226c5e41318e1c113
asari <32879431+deomorxsy@users.noreply.github.com>
2022-10-26 22:51:48 -0300

    add headlines

---
+++ app/headline.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import feedparser
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'fox': 'http://feeds.foxnews.com/foxnews/latest',
+             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+
+@app.route('/')
+@app.route('/<publication>/')
+def get_news(publication='bbc'):
+	feed = feedparser.parse(RSS_FEEDS[publication])
+	first_article = feed['entries'][0]
+	return render_template('home.html', article=feed['entries'])#, articles=feed['entries'])
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+   and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
+
